d4eea0b03df3e6eaa3cf7d184402acdd662d78a0
NitinKhandpa <83783806+NitinKhandpa@users.noreply.github.com>
2021-07-23 12:22:27 +0530

    New

---
+++ Calculator_2ndd.js
@@ -0,0 +1,31 @@
+function show(num){
+        
+    
+    var output= document.getElementById("outputfield"); 
+    output.value+= num;
+
+     }
+     function equal(){
+       var exp= document.getElementById("outputfield").value;
+       
+      
+        var result=  eval(exp);
+      
+  
+        document.getElementById("outputfield").value=result;
+       
+  
+     }
+     function backspace(){
+       var exp= document.getElementById("outputfield").value;
+       
+       length= exp.length;
+       
+      modified= exp.slice(0, (length-1));
+       document.getElementById("outputfield").value=modified;
+       
+     }
+     function clearall(){
+      document.getElementById("outputfield").value=" ";
+      // document.getElementById("outputfield").addAttribute("placeholder", "0");
+     }
